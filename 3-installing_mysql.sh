feb70f08013f28bc085566ed7934c3c244c379c7
charan230199 <venkatasaicharan1999@gmail.com>
2023-12-14 11:30:30 +0530

    mysql

---
+++ 3-installing_mysql.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+ID=$(id-u)
+if [ $ID -ne 0 ]; then
+    echo "you are not root user"
+    exit 1
+else
+    echo "you are root user"
+
+sudo yum install mysql -y
+
+if [ $? -ne 0 ]; then   
+    echo "mysql iss not installed"
+    exit 2
+else
+    echo "mysql is installed"
